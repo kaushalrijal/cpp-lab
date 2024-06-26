558bf14647c8f64eab2459480144c1b65274be46
kaushalrijal <kaushalrijal091@gmail.com>
2024-06-26 21:54:37 +0545

    add problem statement and rename plus a few fixes

---
+++ Lab2/problem1.cpp
@@ -0,0 +1,56 @@
+// To write a C++ program to swap two private data members using friend functions.
+
+#include<iostream>
+
+using namespace std;
+
+class B;
+
+class A{
+    int a;
+    public:
+        void getNum(){
+            cin >> a;
+        }
+
+        void show(){
+            cout << a << endl;
+        }
+
+        friend void swap(A &, B &);
+};
+
+class B{
+    int b;
+    public:
+        void getNum(){
+            cin >> b;
+        }
+
+        void show(){
+            cout << b << endl;
+        }
+
+        friend void swap(A &, B &);
+};
+
+void swap(A &a, B &b){
+    int temp = a.a;
+    a.a = b.b;
+    b.b = temp;
+}
+
+int main(){
+    A aobj;
+    B bobj;
+    
+    cout << "Enter A: ";
+    aobj.getNum();
+    cout << "Enter B: ";
+    bobj.getNum();
+
+    swap(aobj, bobj);
+
+    aobj.show();
+    bobj.show();
+}
