e51b4c9c87f37e41e20fc08e8f4efe58978fede3
kaushalrijal <kaushalrijal091@gmail.com>
2024-06-26 21:49:46 +0545

    add problem for problem 3

--- Lab1/problem3.cpp
+++ Lab1/problem3.cpp
@@ -1,3 +1,13 @@
+/*
+Write a program that checks the order of a medicine from a pharmacy store.
+The program should read the order quantity and the medicine quantity in the
+store. When the order quantity is more than the store quantity, your
+program should display the message “No enough quantity”. When the order
+quantity is less than the store quantity, you have to check that the order
+quantity must be not more than 16 except there is more than 40 items in the
+store. The messages that will be displayed in that cases are “Your order is
+accepted” OR “You cannot order more than 16 item”
+*/
 #include <iostream>
 
 using namespace std;
