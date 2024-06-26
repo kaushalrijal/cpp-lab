154e3f8e2390e22cb514b1282dcddca61899a268
kaushalrijal <kaushalrijal091@gmail.com>
2024-06-26 23:41:57 +0545

    add problem 8

---
+++ Lab2/problem8.cpp
@@ -0,0 +1,20 @@
+/*
+A parking garage charges a $2.00 minimum fee to park for up to three hours. The garage charges an additional
+$0.50 per hour for each hour or part thereof in excess of three hours. People who park their cars for longer
+than 24 hours will pay $8.00 per day. Write a program that calculates and prints the parking charges. The inputs
+to your program are the date and time when a car enters the parking garage, and the dateand time when the same car
+leaves the parking garage. Keep track of number of cars in the garage overloading ++ and -- operators.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+class garage{
+
+};
+
+int main(){
+
+    return 0;
+}
