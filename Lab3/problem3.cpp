f119f0ecee73be3f2630fce695f744a4917e6b7d
kaushalrijal <kaushalrijal091@gmail.com>
2024-08-04 09:51:41 +0545

    fix quadrant issues in problem 3

--- Lab3/problem3.cpp
+++ Lab3/problem3.cpp
@@ -1,37 +1,38 @@
 // Program to convert Cartesian coordinate to Polar.
 
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 
 using namespace std;
 
-class polar{
+class polar {
     float rad, ang;
 
-    public:
-        polar(){}
-        polar(float r, float a) : rad(r), ang(a){}
+public:
+    polar() : rad(0), ang(0) {}
+    polar(float r, float a) : rad(r), ang(a) {}
 
-        void show(){
-            cout << rad << ang;
-        }
+    void show() {
+        cout << "Radius: " << rad << ", Angle: " << ang << " radians" << endl;
+    }
 };
 
-class cartesian{
+class cartesian {
     float x, y;
-    public:
-        cartesian(){}
-        cartesian(float xco, float yco) : x(xco), y(yco){}
 
-        operator polar(){
-            float r = sqrt(x * x + y * y);
-            float a = atan(y/x);
+public:
+    cartesian() : x(0), y(0) {}
+    cartesian(float xco, float yco) : x(xco), y(yco) {}
 
-            return polar(r, a);
-        }
+    operator polar() {
+        float r = sqrt(x * x + y * y);
+        float a = atan2(y, x); // Use atan2 for correct angle calculation
+
+        return polar(r, a);
+    }
 };
 
-int main(){
+int main() {
     cartesian c(2, 3);
     polar p;
 
