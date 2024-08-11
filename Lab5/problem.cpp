fff90c5b9e1fc917cd292005437f06584932f45d
kaushalrijal <kaushalrijal091@gmail.com>
2024-08-11 12:38:53 +0545

    file lab complete

--- Lab5/problem.cpp
+++ Lab5/problem.cpp
@@ -26,35 +26,281 @@ maintaining the catalog data.
 */
 
 #include<iostream>
+#include<fstream>
+#include<conio.h>
 #include<cstring>
 
 using namespace std;
 
 class Library{
-    double ISBN;
+    string ISBN;
     int numberOfCopies;
     char bookTitle[50], author[30];
 
     public:
-        Library(double isbn, int no, char *title, char *a) : ISBN(isbn), numberOfCopies(no){
-            strcpy(bookTitle, title);
-            strcpy(author, a);
-        }
-
         void getData(){
+            cout << "Enter book details" << endl << endl;
             cout << "Enter book ISBN: ";
             cin >> ISBN;
+
             cout << "Enter book title: ";
             cin.ignore();
             cin.getline(bookTitle, 50);
+
             cout << "Enter author name: ";
-            cin.ignore();
             cin.getline(author, 30);
-            cout << "Enter number of copies: ";
+
+            cout << "Enter the number of copies: ";
             cin >> numberOfCopies;
+            cin.ignore();
         }
+
+        void showData(){
+            cout << "Book details" << endl;
+            cout << "ISBN: " << ISBN << endl;
+            cout << "Title: " << bookTitle << endl;
+            cout << "Author: " << author << endl;
+            cout << "No. of copies: " << numberOfCopies << endl;
+        }
+        
+        void addBook() {
+            cout << "Add Book" << endl;
+            getData();
+
+            Library temp;
+            bool match = false;
+            ifstream inf("Library.dat", ios::binary);
+
+            if (!inf) {
+                ofstream outf("Library.dat", ios::binary);
+                if (!outf) {
+                    cerr << "Error creating file!" << endl;
+                    return;
+                }
+                outf.write(reinterpret_cast<const char *>(this), sizeof(*this));
+                outf.close();
+                cout << "Book added successfully!" << endl;
+                return;
+            }
+
+            while (!inf.eof()) {
+                inf.read(reinterpret_cast<char *>(&temp), sizeof(temp));
+                if (temp.ISBN == ISBN) {
+                    match = true;
+                    cout << "ISBN must be unique!" << endl;
+                    break;
+                }
+            }
+            inf.close();
+
+            if (!match) {
+                ofstream outf("Library.dat", ios::app | ios::binary);
+                if (!outf) {
+                    cerr << "Error opening file!" << endl;
+                    return;
+                }
+                outf.write(reinterpret_cast<const char *>(this), sizeof(*this));
+                outf.close();
+                cout << "Book added successfully!" << endl;
+            }
+        
+        }
+
+        void issueBook(){
+            string isbn;
+            cout << "Enter the ISBN of the book to issue: ";
+            cin >> isbn;
+
+            fstream file("Library.dat", ios::binary | ios::in | ios::out);
+            if (!file) {
+                cerr << "Error opening file!" << endl;
+                return;
+            }
+
+            bool found = false;
+            while (file.read(reinterpret_cast<char*>(this), sizeof(*this))) {
+                if (ISBN == isbn) {
+                    found = true;
+                    if (numberOfCopies > 0) {
+                        numberOfCopies--;
+                        file.seekp(-static_cast<int>(sizeof(*this)), ios::cur);
+                        file.write(reinterpret_cast<char*>(this), sizeof(*this));
+                        cout << bookTitle << " issued successfully!" << endl;
+                    } else {
+                        cout << "Sorry, this book is currently unavailable!" << endl;
+                    }
+                    break;
+                }
+            }
+
+            if (!found) {
+                cout << "Book with ISBN " << isbn << " not found!" << endl;
+            }
+
+            file.close();
+        }
+
+        void returnBook(){
+            string isbn;
+            cout << "Enter the ISBN of the book to return: ";
+            cin >> isbn;
+
+            fstream file("Library.dat", ios::in|ios::out|ios::binary);
+            
+            bool found = false;
+            while (file.read(reinterpret_cast<char*>(this), sizeof(*this))) {
+                if (ISBN == isbn) {
+                    found = true;
+                    file.seekp(-static_cast<int>(sizeof(*this)), ios::cur);
+                    file.write(reinterpret_cast<char*>(this), sizeof(*this));
+                    cout << bookTitle << " returned successfully!" << endl;
+                    break;
+                }
+            }
+
+            if (!found) {
+                cout << "Book with ISBN " << isbn << " not found!" << endl;
+            }
+
+            file.close();
+        }
+
+        void updateBook(){
+            string isbn;
+            cout << "Enter the ISBN of the book to update: ";
+            cin >> isbn;
+            cin.ignore();
+
+            fstream file("Library.dat", ios::binary | ios::in | ios::out);
+            if (!file) {
+                cerr << "Error opening file!" << endl;
+                return;
+            }
+
+            bool found = false;
+            while (file.read(reinterpret_cast<char *>(this), sizeof(*this))) {
+                if (ISBN == isbn) {
+                    found = true;
+                    cout << "Update details" << endl;
+                    getData();
+                    file.seekp(-static_cast<int>(sizeof(*this)), ios::cur);
+                    file.write(reinterpret_cast<char *>(this), sizeof(*this));
+                    cout << "Book updated successfully!" << endl;
+                    break;
+                }
+            }
+
+            if (!found) {
+                cout << "Book with ISBN " << isbn << " not found!" << endl;
+            }
+
+            file.close();
+        }
+
+        void deleteBook(){
+            string isbn;
+            cout << "Enter the ISBN of the book to delete: ";
+            cin >> isbn;
+            cin.ignore();
+
+            ifstream inf("Library.dat", ios::binary);
+            if (!inf) {
+                cerr << "Error opening file!" << endl;
+                return;
+            }
+
+            ofstream outf("Temp.dat", ios::binary);
+            if (!outf) {
+                cerr << "Error creating temporary file!" << endl;
+                inf.close();
+                return;
+            }
+
+            bool found = false;
+            while (inf.read(reinterpret_cast<char *>(this), sizeof(*this))) {
+                if (ISBN != isbn) {
+                    outf.write(reinterpret_cast<const char *>(this), sizeof(*this));
+                } else {
+                    found = true;
+                }
+            }
+
+            inf.close();
+            outf.close();
+
+            if (found) {
+                remove("Library.dat");
+                rename("Temp.dat", "Library.dat");
+                cout << "Book deleted successfully!" << endl;
+            } else {
+                cout << "Book with ISBN " << isbn << " not found!" << endl;
+                remove("Temp.dat");
+            }
+        }
+
+        void listBooks() const {
+            cout << "List of books" << endl << endl;
+            ifstream inf("Library.dat", ios::binary | ios::in);
+            if (!inf) {
+                cerr << "Error opening file!" << endl;
+                return;
+            }
+
+            Library temp;
+            while (inf.read(reinterpret_cast<char*>(&temp), sizeof(temp))) {
+                temp.showData();
+                cout << endl;
+            }
+
+            inf.close();
+            cout << "End of list function!" << endl;
+        }
+
 };
 
 int main(){
+    char ch;
+    Library lib;
 
+    while(true){
+        system("cls");
+        cout << "Library Management System\n\n1.Add Book\n2.Issue Book\n3.Return Book\n4.Update Book\n5.Delete Book\n6.List Books\n7.Exit\n\nEnter your choice: ";
+        cin >> ch;
+
+        switch(ch){
+            case '1':
+                lib.addBook();
+                break;
+            
+            case '2':
+                lib.issueBook();
+                break;
+            
+            case '3':
+                lib.returnBook();
+                break;
+            
+            case '4':
+                lib.updateBook();
+                break;
+            
+            case '5':
+                lib.deleteBook();
+                break;
+            
+            case '6':
+                lib.listBooks();
+                break;
+
+            case '7':
+                exit(0);
+            
+            default:
+                cout << "Invalid choice!";
+        }
+
+        cout << endl << "Press any key to continue:";
+        cin.ignore();
+        getch();
+    }
 }
